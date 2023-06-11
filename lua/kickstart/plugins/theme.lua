a2190b1b7baaec5c1e873683d07b085f5e67a386
Nikos Trembois <ntrembois@gmail.com>
2023-06-11 13:36:47 -0700

    added more themes

--- lua/kickstart/plugins/theme.lua
+++ lua/kickstart/plugins/theme.lua
@@ -1,10 +1,18 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
-  config = function()
-    vim.cmd.colorscheme 'tokyonight-storm'
-  end,
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      vim.cmd.colorscheme 'tokyonight-storm'
+    end,
+  },
+  "Mofiqul/dracula.nvim",
+  "ellisonleao/gruvbox.nvim",
+  { "rose-pine/neovim", name = "rose-pine" },
+  "nyoom-engineering/nyoom.nvim",
+  "jacoborus/tender.vim",
+  "rebelot/kanagawa.nvim",
+  { "bluz71/vim-nightfly-colors", name = "nightfly", lazy = false, priority = 1000 },
 }
-
