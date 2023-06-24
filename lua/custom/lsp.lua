75520e585e456c5a55b2666b91d219e46095c823
Nikos Trembois <nikos.trembois@lxl-c57t4w3.na.joby.aero>
2023-06-23 20:12:00 -0700

    modified python lsp and file format

--- lua/custom/lsp.lua
+++ lua/custom/lsp.lua
@@ -1,15 +1,18 @@
 local lsp = {}
 lsp.servers = {
-	pylsp = {
-		pylsp = {
-			plugins = {
-				pycodestyle = {
-					-- ignore = { 'E501' },
-					maxLineLength = 120
-				}
-			}
-		}
-	}
+  pylsp = {
+    pylsp = {
+      plugins = {
+        pycodestyle = {
+          ignore = { 'E501', 'W503', 'W504', 'E704', 'E731' },
+          maxLineLength = 120
+        },
+        jedi_completion = {
+          cache_for = {"pandas","numpy","tensorflow","matplotlib"}
+        }
+      }
+    }
+  },
 }
 
 return lsp
