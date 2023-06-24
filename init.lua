7eec404beb48f9529cf2e30ee7a07bfcb424708a
Nikos Trembois <nikos.trembois@lxl-c57t4w3.na.joby.aero>
2023-06-23 20:06:56 -0700

    fixed working directory

--- init.lua
+++ init.lua
@@ -41,6 +41,7 @@ P.S. You can delete this when you're done too. It's your config now :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+require 'custom.vim-options'
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
