a75178044176737f977fdec2cbafae04e30544a2
Nikos Trembois <ntrembois@gmail.com>
2023-07-07 22:08:46 -0700

    added colorthemes

--- lua/custom/themes.lua
+++ lua/custom/themes.lua
@@ -6,6 +6,9 @@ return {
   "jacoborus/tender.vim",
   "rebelot/kanagawa.nvim",
   "tanvirtin/monokai.nvim",
+  "catppuccin/nvim",
+  "sonph/onehalf",
+  "NLKNguyen/papercolor-theme",
   { "bluz71/vim-nightfly-colors", name = "nightfly", lazy = false, priority = 1000 },
   vim.keymap.set('n', '<leader>ct', require('telescope.builtin').colorscheme, { desc = '[C]hange [T]heme' })
 }
