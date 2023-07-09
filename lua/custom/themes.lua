d3416060976353a5b6c17d10391dd7edd750bc15
Nikos Trembois <ntrembois@gmail.com>
2023-07-09 12:56:22 -0700

    updated themes

--- lua/custom/themes.lua
+++ lua/custom/themes.lua
@@ -1,14 +1,18 @@
 return {
   "Mofiqul/dracula.nvim",
   "ellisonleao/gruvbox.nvim",
-  { "rose-pine/neovim", name = "rose-pine" },
-  "nyoom-engineering/nyoom.nvim",
   "jacoborus/tender.vim",
   "rebelot/kanagawa.nvim",
   "tanvirtin/monokai.nvim",
   "catppuccin/nvim",
   "sonph/onehalf",
   "NLKNguyen/papercolor-theme",
+  "nyoom-engineering/oxocarbon.nvim",
+  { "rose-pine/neovim",           name = "rose-pine" },
   { "bluz71/vim-nightfly-colors", name = "nightfly", lazy = false, priority = 1000 },
-  vim.keymap.set('n', '<leader>ct', require('telescope.builtin').colorscheme, { desc = '[C]hange [T]heme' })
+
+  -- vim.keymap.set('n', '<leader>ct', require('telescope.builtin').colorscheme, { desc = '[C]hange [T]heme' })
+  vim.keymap.set('n', '<leader>ct', function()
+    require('telescope.builtin').colorscheme({ enable_preview = true })
+  end, { desc = '[C]hange [T]heme' })
 }
