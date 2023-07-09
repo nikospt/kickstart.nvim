f429b8721ddd72ed44ef26ffd69d6548708a25a3
Nikos Trembois <ntrembois@gmail.com>
2023-07-09 15:31:15 -0700

    added lsp to status line

--- lua/kickstart/plugins/statusline.lua
+++ lua/kickstart/plugins/statusline.lua
@@ -1,8 +1,12 @@
+local function wrapper()
+  return require("vim.lsp").get_active_clients({bufnr = vim.api.nvim_get_current_buf()})[1].name
+end
 return  {
   -- Set lualine as statusline
   'nvim-lualine/lualine.nvim',
   -- See `:help lualine.txt`
   opts = {
+  sections = { lualine_x = {'encoding', 'fileformat', 'filetype', wrapper} },
     options = {
       icons_enabled = false,
       theme = 'auto',
