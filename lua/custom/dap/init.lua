db0141acd659d10ed399bb44a678f068992b7392
Nikos Trembois <ntrembois@gmail.com>
2023-07-15 14:07:52 -0700

    added a new global debug configuration for pyhon that takes arguments

--- lua/custom/dap/init.lua
+++ lua/custom/dap/init.lua
@@ -8,6 +8,17 @@ dap.configurations.python = {
 		cwd = '${workspaceFolder}',
 		stopOnEntry = true,
 	},
+	{
+		name = "current file with args",
+		type = "python",
+		request = "launch",
+		program = "${file}",
+		cwd = '${workspaceFolder}',
+		args = {function ()
+			return vim.fn.input('Input file: ', vim.fn.getcwd() .. '/', 'file')
+		end},
+		stopOnEntry = true,
+	},
 }
 local dap_configs = require 'custom.dap.configs'
 for k, v in pairs(dap_configs.python) do
