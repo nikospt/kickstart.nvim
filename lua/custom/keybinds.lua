ea424e4ac1e799429fe670af3e7869d8df3b696a
Nikos Trembois <ntrembois@gmail.com>
2023-07-29 10:16:25 -0700

    increase timeout time of lsp formatter

--- lua/custom/keybinds.lua
+++ lua/custom/keybinds.lua
@@ -1,5 +1,5 @@
 local nmap = function(keys, func, desc)
-	vim.keymap.set('n', keys, func, {desc = desc})
+	vim.keymap.set('n', keys, func, { desc = desc })
 end
 
 local clear_highlights = function()
@@ -11,9 +11,8 @@ vim.keymap.set('n', '<leader>dd', require('neogen').generate)
 
 return {
 	nmap('<leader>ch', clear_highlights, '[C]lear [H]ightlights'),
-	-- I should increase timeout_ms on the code format function 
-	-- something like vim.lsp.buf.format({timeout_ms=10000})
-	nmap('<leader>cf', vim.lsp.buf.format, 'LSP: [C]ode [F]ormat'),
+	-- Allow the codeformatter to run for 10,000 ms (10s) before timing out
+	nmap('<leader>cf', function() vim.lsp.buf.format({ timeout_ms = 10000 }) end, 'LSP: [C]ode [F]ormat'),
 	nmap('<leader>ss', vim.lsp.buf.document_highlight, 'LSP [S]cope [S]ymbols'),
 	nmap('<leader>ts', require('telescope.builtin').treesitter, '[T]ree [S]itter'),
 	nmap('<leader>tds', function() vim.cmd('SymbolsOutline') end, '[T]reeview of [D]ocument [S]ymbols')
