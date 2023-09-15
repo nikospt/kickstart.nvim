db5174c4dc8cd6e04957ca7e6c6452870623e281
Nikos Trembois <ntrembois@gmail.com>
2023-09-15 05:48:11 -0700

    improved neogen

--- lua/custom/config/neogen.lua
+++ lua/custom/config/neogen.lua
@@ -2,37 +2,38 @@ local M = {}
 
 local i = require("neogen.types.template").item
 local py_annotation = {
-	{ nil,         '"""',            { type = { "func" } } },
-	{ nil,         '<description>',  { type = { "func" } } },
-	{ i.Parameter, ":param %s:$1", },
-	-- { i.Return,    ":return:$1|any" },
-	{ nil,         '"""',            { type = { "func" } } },
+	{ nil,         '"""',               { type = { "func" } } },
+	{ nil,         '$1',                { type = { "func" } } },
+	{ i.Parameter, ":param %s: $1|any", },
+	{ i.Tparam,    ":param %s: $1|any", },
+	{ i.Return,    ":return: $1|any" },
+	{ nil,         '"""',               { type = { "func" } } },
 }
 
 function M.setup()
 	require("neogen").setup({
-	-- Enables placeholders when inserting annotation
-      enable_placeholders = true,
-		snippet_engine='luasnip',
+		-- Enables placeholders when inserting annotation
+		enable_placeholders = true,
+		snippet_engine = 'luasnip',
 
-      -- Placeholders used during annotation expansion
-      placeholders_text = {
-          ["description"] = "[TODO:description]",
-          ["tparam"] = "[TODO:tparam]",
-          ["parameter"] = "[TODO:parameter]",
-          ["return"] = "[TODO:return]",
-          ["class"] = "[TODO:class]",
-          ["throw"] = "[TODO:throw]",
-          ["varargs"] = "[TODO:varargs]",
-          ["type"] = "[TODO:type]",
-          ["attribute"] = "[TODO:attribute]",
-          ["args"] = "[TODO:args]",
-          ["kwargs"] = "[TODO:kwargs]",
-      },
+		-- Placeholders used during annotation expansion
+		placeholders_text = {
+			["description"] = "[TODO:description]",
+			["tparam"] = "[TODO:tparam]",
+			["parameter"] = "[TODO:parameter]",
+			["return"] = "[TODO:return]",
+			["class"] = "[TODO:class]",
+			["throw"] = "[TODO:throw]",
+			["varargs"] = "[TODO:varargs]",
+			["type"] = "[TODO:type]",
+			["attribute"] = "[TODO:attribute]",
+			["args"] = "[TODO:args]",
+			["kwargs"] = "[TODO:kwargs]",
+		},
 
-      -- Placeholders highlights to use. If you don't want custom highlight, pass "None"
-      placeholders_hl = "DiagnosticHint",
-	enabled = true,
+		-- Placeholders highlights to use. If you don't want custom highlight, pass "None"
+		placeholders_hl = "DiagnosticHint",
+		enabled = true,
 		languages = {
 			python = {
 				template = {
